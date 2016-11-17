354627e51ff378903a8c6c180c0df2a58afe03fc
Christoph Matthies <christoph.matthies@gmail.com>
2016-11-17 13:12:01 +0100

    Remove/Refactor skipped tests.

--- test.py
+++ test.py
@@ -261,30 +261,24 @@ class TestDiffMultipleFiles(MarvinTest):
 		for e in expected:
 			self.assertIn(e, single_file_changes)
 
-@unittest.skip("Not refactored yet")
 class TestDiffLarge(MarvinTest):
 	def setUp(self):
 		self.file_changes = self.setup_parser('pr_338.diff').parse()
 
 	def test_amount(self):
-		self.assertLength(self.file_changes, 15)
+		self.assertTrue(len(self.file_changes) > 30)
 
 	def test_selected_changes(self):
-		changes = [c['changes'] for c in self.file_changes if c['header'].new_path=='app/controllers/work_days_controller.rb'].pop()
-		actual = [
-			{'end': 16, 'start': 11, 'type': 'delete'},
-			{'end': 24, 'start': 11, 'type': 'insert'},
-			{'end': 20, 'start': 19, 'type': 'delete'},
-			{'end': 27, 'start': 27, 'type': 'insert'},
-			{'end': 32, 'start': 32, 'type': 'insert'},
-			{'end': 37, 'start': 37, 'type': 'insert'},
-			{'end': 59, 'start': 59, 'type': 'delete'},
-			{'end': 68, 'start': 68, 'type': 'insert'},
-			{'end': 76, 'start': 73, 'type': 'delete'},
-			{'end': 78, 'start': 78, 'type': 'delete'},
-			{'end': 83, 'start': 83, 'type': 'insert'}
-		]
-		self.assertCountEqual(actual, changes)
+		file = "app/views/time_sheets/edit.html.erb"
+		new_sha = "9b41892"
+		single_file_changes = [c for c in self.file_changes if c.file_path == file]
+		# Deleted line 5, line 6-13 added
+		expected = [LineChange(line_number=5, commit_sha=new_sha,
+						file_path=file, change_type=LineChange.ChangeType.modified),
+					LineChange(line_number=12, commit_sha=new_sha,
+						file_path=file, change_type=LineChange.ChangeType.added)]
+		for e in expected:
+			self.assertIn(e, single_file_changes)
 
 class TestBlame(MarvinTest):
 	def setUp(self):
@@ -324,59 +318,6 @@ class TestBlame(MarvinTest):
 		blame_info_after = self.blamer.blame_line(self.line_count + 1)
 		self.assertIsNone(blame_info_after)
 		logging.disable(logging.NOTSET)
-
-@unittest.skip("Not refactored yet")
-class TestBlameInsert(MarvinTest):
-	def setUp(self):
-		self.line = 10
-		file_changes = [
-			{'changes': [{'end': self.line, 'start': self.line, 'type': 'insert'}],
-			'header': header(index_path=None, old_path='Gemfile',
-				old_version='647ad8d', new_path='Gemfile', new_version='de30360')}]
-		self.file_blames = self.marvin.blame_changes(file_changes)
-
-	def test_file(self):
-		self.assertLength(self.file_blames, 1)
-		self.assertIn('Gemfile', self.file_blames)
-
-	def test_blame(self):
-		blame = self.file_blames.popitem()[1]
-		actual = [{'type': 'insert',
-				'author': 'jaSunny',
-				'timestamp': '1444140126',
-				'commit': '867c8f83d432a5c8d7236735e513a3bd0b12bb38',
-				'lines': {self.line-1, self.line+1}}]
-		self.assertEqual(actual, blame)
-
-@unittest.skip("Not refactored yet")
-class TestBlameMultipleInserts(MarvinTest):
-	def setUp(self):
-		file_changes = [{'changes': [
-			{'end': 24, 'start': 11, 'type': 'insert'},
-			{'end': 27, 'start': 27, 'type': 'insert'},
-			{'end': 32, 'start': 32, 'type': 'insert'},
-			{'end': 37, 'start': 37, 'type': 'insert'},
-			{'end': 68, 'start': 68, 'type': 'insert'},
-			{'end': 83, 'start': 83, 'type': 'insert'}],
-		'header': header(
-			index_path=None,
-			old_path='app/controllers/work_days_controller.rb',
-			old_version='6ace82e',
-			new_path='app/controllers/work_days_controller.rb',
-			new_version='70629c2')}]
-		self.file_blames = self.marvin.blame_changes(file_changes)
-
-	def test_blame(self):
-		actual = [	{'commit': '8342fefcf6d5e8cb4d834a95576b4237768fcd80', 'author': 'Moritz Eissenhauer', 'timestamp': '1447859591', 'type': 'insert', 'lines': {33, 36, 69, 38, 10, 31}},
-					{'commit': '0c82d240f32026caf6c6238d1d7204e7766f723a', 'author': 'Jonas Bounama', 'timestamp': '1452693891', 'type': 'insert', 'lines': {25}},
-					{'commit': 'b21818bd1be06f959c5fb9916112c6889ab8f9fc', 'author': 'Lennard Wolf', 'timestamp': '1448723686', 'type': 'insert', 'lines': {26, 28}},
-					{'commit': '2b573f4afeac83a59b741483fc067839d952bd5c', 'author': 'Moritz Eissenhauer', 'timestamp': '1449840415', 'type': 'insert', 'lines': {67}},
-					{'commit': '04fe0c5b7a3cbad49c6847e302591a1b7c6dc8d9', 'author': 'Soeren Tietboehl', 'timestamp': '1454418775', 'type': 'insert', 'lines': {82, 84}}]
-		# import pdb; pdb.set_trace()
-		print([y for x,y in self.file_blames.items()][0])
-		for file, blame_list in self.file_blames.items():
-			for blame in blame_list:
-				self.assertIn(blame, actual)
 
 
 if __name__ == '__main__':
